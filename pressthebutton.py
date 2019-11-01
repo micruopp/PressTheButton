86b458d280015719d2d6faf7556cc545fc568d15
Michal Ruopp <micruopp@gmail.com>
2019-11-01 01:49:14 -0500

    Debugging...

--- pressthebutton.py
+++ pressthebutton.py
@@ -61,7 +61,7 @@ def index():
 		return data
 
 	elif request.method == 'GET':
-		return render_template('layout.html', stylesheets=stylesheets, scripts=scripts, count=count)
+		return render_template('index.html', stylesheets=stylesheets, scripts=scripts, count=count)
 
 if __name__ == "__main__":
 	app.run(host='127.0.0.1', port='5005')
