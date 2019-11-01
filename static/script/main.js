01927fbf1071960bbe5c78ea3bdf07ef914d63be
Michal Ruopp <micruopp@gmail.com>
2019-11-01 12:18:37 -0500

    Debugging...

--- static/script/main.js
+++ static/script/main.js
@@ -7,34 +7,35 @@ function main() {
 	let app = new Application();
 	app.log("Hello, world!");
 
-	// let socket = io.connect('http://' + document.domain + ':' + location.port + '/');
+	let path = '/launchpad/pressthebutton/';
+	let socket = io.connect('http://' + document.domain + ':' + location.port + path);
 
 	let button = document.querySelector('#the-button');
 	button.addEventListener('click', function(e) {
 		app.log("Click!");
 		let url = '/launchpad/pressthebutton';
 
-		// socket.emit('clicked', {});
+		socket.emit('clicked', {});
 
-		app.request(url, function(res, err) {
-			let count = res;
-			updateCountLabel(count);
-		});
+		// app.request(url, function(res, err) {
+		// 	let count = res;
+		// 	updateCountLabel(count);
+		// });
 	});
 
-	// button.addEventListener('touchstart', function(e) {
-	// 	let label = document.querySelector('#count');
-	// 	label.innerHTML = "TOUCHED";
-	// });
+	button.addEventListener('touchstart', function(e) {
+		let label = document.querySelector('#count');
+		label.innerHTML = "TOUCHED";
+	});
 
-	// socket.on('connection', function(msg) {
-	// 	console.log("Socket connected.");
-	// });
+	socket.on('connection', function(msg) {
+		console.log("Socket connected.");
+	});
 
-	// socket.on('count update', function(msg) {
-	// 	let count = msg['count'];
-	// 	updateCountLabel(count);
-	// });
+	socket.on('count update', function(msg) {
+		let count = msg['count'];
+		updateCountLabel(count);
+	});
 
 	// Emit
 
